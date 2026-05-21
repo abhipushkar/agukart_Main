8ce3165096d0564e7386b91ed8e7a398f86ce964
Uday <udaytinker1626@gmail.com>
2026-05-21 18:48:31 +0530

    Added Meta Title, Meta Description, Meta Keywords in footer links

--- src/app/(layout-1)/top-brands/page.jsx
+++ src/app/(layout-1)/top-brands/page.jsx
@@ -1,17 +1,39 @@
 import TopBrands from "components/topBrands/TopBrands";
 import React from "react";
+import axios from "axios";
 
-export const metadata = {
-  title: "Our Top Brands - Agukart Next.js E-commerce Template",
-  description: `Agukart is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
-  authors: [
-    {
-      name: "UI-LIB",
-      url: "https://ui-lib.com",
-    },
-  ],
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react"],
-};
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export async function generateMetadata() {
+  try {
+    const res = await axios.post(
+      `${baseURL}/get-description`,
+      {
+        type: "Our Top Brands",
+      }
+    );
+
+    const data = res?.data?.information;
+
+    return {
+      title: `Our Top Brands - ${data?.meta_title || "Our Top Brands"}`,
+      description: data?.meta_description || "",
+      keywords: data?.meta_keywords || [],
+      authors: [
+        {
+          name: "UI-LIB",
+          url: "https://ui-lib.com",
+        },
+      ],
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      title: "Our Top Brands",
+    };
+  }
+}
 
 const page = () => {
   return <TopBrands />;
--- src/app/(layout-1)/top-stores/page.jsx
+++ src/app/(layout-1)/top-stores/page.jsx
@@ -1,17 +1,39 @@
 import TopStores from "components/topStores/TopStores";
 import React from "react";
+import axios from "axios";
 
-export const metadata = {
-  title: "Our Top Stores - Agukart Next.js E-commerce Template",
-  description: `Agukart is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
-  authors: [
-    {
-      name: "UI-LIB",
-      url: "https://ui-lib.com",
-    },
-  ],
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react"],
-};
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export async function generateMetadata() {
+  try {
+    const res = await axios.post(
+      `${baseURL}/get-description`,
+      {
+        type: "Our Top Store",
+      }
+    );
+
+    const data = res?.data?.information;
+
+    return {
+      title: `Our Top Stores - ${data?.meta_title || "Our Top Stores"}`,
+      description: data?.meta_description || "",
+      keywords: data?.meta_keywords || [],
+      authors: [
+        {
+          name: "UI-LIB",
+          url: "https://ui-lib.com",
+        },
+      ],
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      title: "Our Top Stores",
+    };
+  }
+}
 
 const page = () => {
   return <TopStores />;
